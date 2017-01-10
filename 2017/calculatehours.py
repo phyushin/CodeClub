e584aac48b11c6cd50dedb2f94cae46e47f68e11
Paul <Phyushin@gmail.com>
2017-01-10 13:04:22 +0000

    added solution

--- 2017/calculatehours.py
+++ 2017/calculatehours.py
@@ -1,11 +1,20 @@
+#note this is definately not the *correct* way to do this - it's done this way to show the logic
 hour = 60
-def convert_mins_to_hours(mins):
+day = 24 * hour
+def calculate_remaining_mins_days(mins):
+    remaining_mins = mins % day
+    return remaining_mins
+
+def calculate_remaining_mins_hours(mins):
     remaining_mins = mins % hour
     full_hours = (mins-remaining_mins)/60
     return full_hours
 
 total_mins = int(input("please enter an amount of minutes: "))
-total_hours = convert_mins_to_hours(total_mins) #this gets us the hours
-remainder_mins = total_mins - (total_hours*hour)#this gets us the remaining mins
 
-print("{0} hours and {1} minute(s)".format(int(total_hours),int(remainder_mins)))
+days = (total_mins - calculate_remaining_mins_days(totalmins)/day)
+
+hours = (calculate_remaining_mins_hours(total_mins-(days*day)))
+
+mins = total_mins -((day*days)+(hours*hour))
+print("{0} days {1}hour(s) and {2} minute(s)".format(int(days),int(total_hours),int(remainder_mins)))
